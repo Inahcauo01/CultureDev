347aee60f0dc15a27002a64d967e27f5072dba63
tarek <tarek.ouachani01@gmail.com>
2023-01-13 19:40:08 +0000

    sign in & sign up front end

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,9 @@
 
-let eye      = document.querySelectorAll(".eye");
-let inputPwd = document.querySelector("#input-pwd");
+let eye       = document.querySelectorAll(".eye");
+let inputPwd  = document.querySelector("#input-pwd");
+let inputs    = document.querySelectorAll(".form-control")
+let switchLog = document.querySelectorAll(".click-ici")
+let container = document.querySelector(".container");
 
 eye.forEach(eye => {
     eye.addEventListener("click",()=>{
@@ -14,6 +17,22 @@ eye.forEach(eye => {
             document.querySelector(".eye-on").classList.remove("hide")
             inputPwd.setAttribute("type","password");
         }
-
     })
+});
+
+switchLog.forEach(switchS => {
+    switchS.addEventListener("click",()=>{
+    if(switchS.classList.contains("btn-signin")){
+        document.querySelector(".sign-up").classList.remove("hide")
+        document.querySelector(".sign-in").classList.add("hide")
+        document.querySelector(".image-signup").classList.add("hide")
+        document.querySelector(".image-login").classList.remove("hide")
+
+    }else{
+        document.querySelector(".sign-in").classList.remove("hide")
+        document.querySelector(".sign-up").classList.add("hide")
+        document.querySelector(".image-signup").classList.remove("hide")
+        document.querySelector(".image-login").classList.add("hide")
+    }
+})
 });
